552c987a0cadf077cb54e8d7a1671d8e6aa6ebdb
brakdag <brakdag@gmail.com>
2020-05-10 22:15:37 -0300

    casi terminado

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
-
 const router = express.Router();
 
-router.get('/intradiario', function (req, res) {
+var bufer=""
+
+router.get('/set', function (req, res) {
+bufer = req.query.code
+//console.log(bufer)
+res.send(true)
 });
+
+router.get('/get', function (req, res) {
+//console.log(bufer)
+res.send(encodeURI(bufer))
+});
+
+
 
 router.get('/', (req, res) => { res.render('index.ejs') })
 router.get('/pizarra', (req, res) => { res.render('pizarra.ejs') })
+router.get('/md', (req, res) => { res.render('md.ejs') })
+router.get('/output', (req, res) => { res.render('output.ejs') })
+
 
 module.exports=router
